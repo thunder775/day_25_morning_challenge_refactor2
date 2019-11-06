d820b975976748647887c7f28af9dd1b5aa606b8
McLarenCollege <hello@mclarencollege.in>
2019-11-06 08:27:04 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,34 @@
+// Challenge 1
+// Write Test cases for given challenges
+
+// Challenge 2
+// Sudoku Validation
+// Write a sudoku validator. This function should return true if the 2-D array represents a valid sudoku and false otherwise. To be a valid sudoku:
+//
+// - Each row must have the digits from 1 to 9 exactly once.
+// - Each column must have the digits from 1 to 9 exactly once.
+// - Each 3x3 box must have the digits from 1 to 9 exactly once.
+// Examples
+//  sudokuValidator([
+//  [ 1, 5, 2, 4, 8, 9, 3, 7, 6 ],
+//  [ 7, 3, 9, 2, 5, 6, 8, 4, 1 ],
+//  [ 4, 6, 8, 3, 7, 1, 2, 9, 5 ],
+//  [ 3, 8, 7, 1, 2, 4, 6, 5, 9 ],
+//  [ 5, 9, 1, 7, 6, 3, 4, 2, 8 ],
+//  [ 2, 4, 6, 8, 9, 5, 7, 1, 3 ],
+//  [ 9, 1, 4, 6, 3, 7, 5, 8, 2 ],
+//  [ 6, 2, 5, 9, 4, 8, 1, 3, 7 ],
+//  [ 8, 7, 3, 5, 1, 2, 9, 6, 4 ]
+//  ]) ➞ true
+
+// Challenge 3
+// Sort by Factor Length
+// A numbers factor length is simply its total number of factors.
+// Create a function that sorts an array by factor length in descending order.
+// If multiple numbers have the same factor length, sort these numbers in descending order, with the largest first.
+//
+// In the example below, since 13 and 7 both have only 2 factors, we put 13 ahead of 7.
+//
+// factorSort([9, 7, 13, 12]) ➞ [12, 9, 13, 7]
+main() {
+}
