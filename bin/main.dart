32fbf94d13192d0ccd6661ed4f0d2bc82dc27273
thunder775 <rahulmasih61@gmail.com>
2019-11-13 15:10:45 +0530

    refactored

--- bin/main.dart
+++ bin/main.dart
@@ -9,18 +9,19 @@
 // - Each column must have the digits from 1 to 9 exactly once.
 // - Each 3x3 box must have the digits from 1 to 9 exactly once.
 // Examples
-//  sudokuValidator([
-//  [ 1, 5, 2, 4, 8, 9, 3, 7, 6 ],
-//  [ 7, 3, 9, 2, 5, 6, 8, 4, 1 ],
-//  [ 4, 6, 8, 3, 7, 1, 2, 9, 5 ],
-//  [ 3, 8, 7, 1, 2, 4, 6, 5, 9 ],
-//  [ 5, 9, 1, 7, 6, 3, 4, 2, 8 ],
-//  [ 2, 4, 6, 8, 9, 5, 7, 1, 3 ],
-//  [ 9, 1, 4, 6, 3, 7, 5, 8, 2 ],
-//  [ 6, 2, 5, 9, 4, 8, 1, 3, 7 ],
-//  [ 8, 7, 3, 5, 1, 2, 9, 6, 4 ]
-//  ]) ➞ true
-bool sudokuValidator(List<List> board) {
+List<List<int>> board = [
+  [1, 5, 2, 4, 8, 9, 3, 7, 6],
+  [7, 3, 9, 2, 5, 6, 8, 4, 1],
+  [4, 6, 8, 3, 7, 1, 2, 9, 5],
+  [3, 8, 7, 1, 2, 4, 6, 5, 9],
+  [5, 9, 1, 7, 6, 3, 4, 2, 8],
+  [2, 4, 6, 8, 9, 5, 7, 1, 3],
+  [9, 1, 4, 6, 3, 7, 5, 8, 2],
+  [6, 2, 5, 9, 4, 8, 1, 3, 7],
+  [8, 7, 3, 5, 1, 2, 9, 6, 4]
+];
+
+bool sudokuValidator(List<List<int>> board) {
   return isSudokuBoardOfRightSize(board) &&
       doCellsContainValidNumber(board) &&
       checkRowCol(board) &&
@@ -29,50 +30,24 @@ bool sudokuValidator(List<List> board) {
 
 //lets check if the board is valid or not first
 // that is, size of the board should be multiple of 3
-bool isSudokuBoardOfRightSize(List<List> board) {
-  for (List row in board) {
-    if (row.length % 3 != 0 && board.length % 3 != 0) {
-      return false;
-    }
-  }
-  print('right size');
-  return true;
-}
+bool isSudokuBoardOfRightSize(List<List<int>> board) =>
+    board.every((row) => row.length % 3 == 0 && board.length == row.length);
 
 //now lets check if the sudokuBoard contains valid numbers as per its size
-bool doCellsContainValidNumber(List<List> board) {
-  int length =
-      board.length; //length of board or number of rows in board and their size
-  //so all numbers should be form 0 to length
-  for (int row = 0; row < length; row++) {
-    for (int col = 0; col < length; col++) {
-      if (board[row][col] > length ||
-          board[row][col] < 0 ||
-          board[row][col] == 0) {
-        return false;
-      }
-    }
-  }
-  print('valid numbers');
-  return true;
-}
+bool doCellsContainValidNumber(List<List<int>> board) => board
+    .expand((row) => row)
+    .toList()
+    .every((element) => element > 0 && element < board.length + 1);
 
 //now lets check each row and also each column for duplicates
-bool checkRowCol(List<List> board) {
+bool checkRowCol(List<List<int>> board) {
   int length = board.length;
-  //checking rows first
-  for (int row = 0; row < length; row++) {
-    if (board[row].toSet().length != length) {
-      return false;
-    }
+  if (board.any((row) => row.toSet().length != board.length)) {
+    return false;
   }
-  //checking columns
   for (int col = 0; col < length; col++) {
-    List colElementsList = [];
-    for (int row = 0; row < length; row++) {
-      colElementsList.add(board[row][col]);
-    }
-    if (colElementsList.toSet().length != length) {
+    List colElements = board.map((List row) => row[col]).toList();
+    if (colElements.toSet().length != length) {
       return false;
     }
   }
@@ -82,10 +57,9 @@ bool checkRowCol(List<List> board) {
 
 //now lets check if all the subBoards of size 3
 //don't have any repeating numbers
-bool checkBoxes(List<List> board) {
+bool checkBoxes(List<List<int>> board) {
   for (int row = 0; row < board.length; row = row + 3) {
     for (int col = 0; col < board.length; col = col + 3) {
-//      print('row $row col $col');
       if (!isBoardBoxValid(board, row, col)) {
         return false;
       }
@@ -95,16 +69,16 @@ bool checkBoxes(List<List> board) {
   return true;
 }
 
-bool isBoardBoxValid(List<List> board, int rowPos, int colPos) {
+bool isBoardBoxValid(List<List<int>> board, int rowPos, int colPos) {
   List elements = [];
+  List elementss = board.sublist(rowPos,rowPos+3).map((List<int> row)=>row.sublist(colPos,colPos+3)).expand((List<int> miniRow)=> miniRow).toList();
 
   for (int row = rowPos; row < rowPos + 3; row++) {
     for (int col = colPos; col < colPos + 3; col++) {
       elements.add(board[row][col]);
     }
   }
-//  print(elements); // debug print statement
-  if (elements.toSet().length != board.length) {
+  if (elementss.toSet().length != board.length) {
     return false;
   }
   return true;
@@ -135,14 +109,6 @@ int countFactors(int number) {
 
 main() {
 //  print(countElementInList([1, 5, 2, 4, 1, 9, 3, 7, 6], 1));
-  List<List> board = [
-    [
-      1,
-      5,
-      2,
-    ],
-    [7, 3, 9],
-    [4, 6, 8],
-  ];
+
   print(sudokuValidator(board));
 }
